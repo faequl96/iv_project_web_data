10faded2d85e30dc67facdfbd72c6ddcba6dc324
faiq <faiq@voltras.co.id>
2026-01-05 22:07:27 +0700

    up

--- lib/src/rsvp/rsvp_cubit.dart
+++ lib/src/rsvp/rsvp_cubit.dart
@@ -17,16 +17,37 @@ class RSVPCubit extends Cubit<RSVPState> {
     try {
       emit(state.copyWith(isLoadingCreate: true, rsvp: null.toCopyWithValue()));
 
-      final url = Uri.parse('${ApiConfig.url}/rsvp');
+      final url = Uri.parse('${ApiUrl.value}/rsvp');
       final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json', 'ngrok-skip-browser-warning': 'true'},
         body: jsonEncode(request.toJson()),
       );
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         final RSVPResponse rsvp = RSVPResponse.fromJson(jsonDecode(response.body)['data']);
 
-        emit(state.copyWith(isLoadingCreate: false, rsvp: rsvp.toCopyWithValue()));
+        final updatedRSVPs = <RSVPResponse>[];
+        for (final item in state.rsvps ?? <RSVPResponse>[]) {
+          if (item.invitedGuest.id == rsvp.invitedGuest.id) {
+            updatedRSVPs.add(
+              RSVPResponse(
+                id: item.id,
+                invitedGuest: rsvp.invitedGuest,
+                message: item.message,
+                createdAt: item.createdAt,
+                nickname: item.nickname,
+                avatar: item.avatar,
+                possiblePresence: item.possiblePresence,
+              ),
+            );
+          } else {
+            updatedRSVPs.add(item);
+          }
+        }
+
+        final newRSVPs = [rsvp, ...updatedRSVPs];
+
+        emit(state.copyWith(isLoadingCreate: false, rsvp: rsvp.toCopyWithValue(), rsvps: newRSVPs.toCopyWithValue()));
 
         return true;
       }
@@ -45,8 +66,7 @@ class RSVPCubit extends Cubit<RSVPState> {
     try {
       emit(state.copyWith(isLoadingGetsByInvitationId: true, rsvps: null.toCopyWithValue()));
 
-      await Future.delayed(const Duration(seconds: 8));
-      final url = Uri.parse('${ApiConfig.url}/rsvps/invitation-id/$invitationId');
+      final url = Uri.parse('${ApiUrl.value}/rsvps/invitation-id/$invitationId');
       final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
       if (response.statusCode == 200) {
         final List<RSVPResponse> rsvps = (jsonDecode(response.body)['data'] as List)
